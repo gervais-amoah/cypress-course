789b22d1d8984d6d6906fb7f2240d8bb97ee4620
Gervais Amoah <gervaisamoah@yahoo.com>
2023-12-02 11:43:21 +0000

    Click test is not working

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,5 +1,23 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Fundamentals test", () => {
+  // beforeEach(() => cy.visit("/fundamentals"));
+
+  it("Should open and close the first accordion & Contains correct header text", () => {
+    cy.visit("/fundamentals");
+    cy.get('[data-test="fundamentals-header"]').should(
+      "contain.text",
+      "Testing Fundamentals"
+    );
+    cy.get('[data-test="testing-btn"]').should("be.visible");
+    cy.get('[data-test="testing-btn"]').should("be.visible").click();
+    cy.contains(/Cypress gives you various commands to help you test/i).should(
+      "not.be.visible"
+    );
+    cy.get('[data-test="accordion-item-3"] > div[role="button"]')
+      .should("be.visible")
+      .focus()
+      .click();
+    cy.contains(/Cypress gives you various commands to help you test/i).should(
+      "be.visible"
+    );
+  });
+});
